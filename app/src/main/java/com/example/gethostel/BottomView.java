98c2e43b663b3a7f545c1eb457680faecc80f545
MGrayce <amgraymofficial@gmail.com>
2019-04-10 08:45:01 +0300

    Implicit Intents added

--- app/src/main/java/com/example/gethostel/BottomView.java
+++ app/src/main/java/com/example/gethostel/BottomView.java
@@ -1,15 +1,19 @@
 package com.example.gethostel;
 
 
+import android.content.Intent;
 import android.support.v4.app.Fragment;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class BottomView extends AppCompatActivity {
-
+    Button tp,bc,gf,mk;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,6 +22,43 @@ public class BottomView extends AppCompatActivity {
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
         bottomNavigationView.setOnNavigationItemSelectedListener(navListener);
         getSupportFragmentManager().beginTransaction().replace(R.id.frame,new HomeFragment()).commit();
+
+        tp = findViewById(R.id.tp);
+//        tp.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Intent tp = new Intent(getApplicationContext(),Triple.class);
+//                startActivity(tp);
+//                Toast.makeText(getApplicationContext(), "Triple", Toast.LENGTH_SHORT).show();
+//            }
+//        });
+        bc = findViewById(R.id.bc);
+//        bc.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Intent bc = new Intent(getApplicationContext(),Beco.class);
+//                startActivity(bc);
+//                Toast.makeText(getApplicationContext(), "Beco", Toast.LENGTH_SHORT).show();
+//            }
+//        });
+        gf = findViewById(R.id.gf);
+//        gf.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Intent gf = new Intent(getApplicationContext(),Golf.class);
+//                startActivity(gf);
+//                Toast.makeText(getApplicationContext(), "Golf", Toast.LENGTH_SHORT).show();
+//            }
+//        });
+        mk = findViewById(R.id.mk);
+//        mk.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Intent mk = new Intent(getApplicationContext(),Mark.class);
+//                startActivity(mk);
+//                Toast.makeText(getApplicationContext(), "Mark", Toast.LENGTH_SHORT).show();
+//            }
+//        });
 
 
     }
@@ -38,4 +79,22 @@ public class BottomView extends AppCompatActivity {
             return true;
         }
     };
+    public void Triple(View view) {
+        Toast.makeText(this, "Triple", Toast.LENGTH_SHORT).show();
+        Intent tp = new Intent(this,Triple.class);
+        startActivity(tp);
+
+    }
+    public void Mark(View view) {
+        Intent mk = new Intent(this,Mark.class);
+        startActivity(mk);
+    }
+    public void Beco(View view){
+        Intent bc = new Intent(this,Beco.class);
+        startActivity(bc);
+    }
+    public void Golf(View view) {
+        Intent gf = new Intent(this,Golf.class);
+        startActivity(gf);
+    }
 }
---
+++ app/src/main/java/com/example/gethostel/Golf.java
@@ -0,0 +1,33 @@
+package com.example.gethostel;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class Golf extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_golf);
+    }
+    public void callOwner(View view) {
+        Intent intentCall = new Intent(Intent.ACTION_CALL);
+        intentCall.setData(Uri.parse("tel:0789341535"));
+        if(ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.CALL_PHONE)!= PackageManager.PERMISSION_GRANTED){
+            Toast.makeText(getApplicationContext(), "Please grant the application Permission Access", Toast.LENGTH_SHORT).show();
+            requestPermission();
+        }else{
+            startActivity(intentCall);
+        }
+    }
+    private void requestPermission(){
+        ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.CALL_PHONE},1);
+    }
+}
